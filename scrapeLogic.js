af3c750c42d821d7c35720dae3eff7d96ea15598
letr94 <123594602+letr94@users.noreply.github.com>
2024-09-30 01:15:25 -0400

    First commit

---
+++ scrapeLogic.js
@@ -0,0 +1,45 @@
+const { response } = require("express");
+const puppeteer = require("puppeteer");
+const scrapeLogic = async (res) => {
+
+      
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch();
+    try{
+   
+        throw new Error("whopps");
+    const page = await browser.newPage();
+    
+    // Navigate the page to a URL.
+    await page.goto('https://developer.chrome.com/');
+    
+    // Set screen size.
+    await page.setViewport({width: 1080, height: 1024});
+    
+    // Type into search box.
+    await page.locator('.devsite-search-field').fill('automate beyond recorder');
+    
+    // Wait and click on first result.
+    await page.locator('.devsite-result-item-link').click();
+    
+    // Locate the full title with a unique string.
+    const textSelector = await page
+      .locator('text/Customize and automate')
+      .waitHandle();
+    const fullTitle = await textSelector?.evaluate(el => el.textContent);
+    
+    // Print the full title.
+    console.log('The title of this blog post is "%s".', fullTitle);
+    res.send(fullTitle);
+    } catch (e){
+        console.error(e);
+        res.send(`something went wrong while running puppetter: ${e}`)
+    } finally  {
+      await browser.close();
+
+    }
+  
+
+};
+
+module.exports = {scrapeLogic};
